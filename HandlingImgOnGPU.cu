709ad1542dae363d360d10cd55448e2e361d2c85
PetrB09 <petrb1996@gmail.com>
2017-03-13 22:23:37 +0300

    change all functions to workable condition

--- HandlingImgOnGPU.cu
+++ HandlingImgOnGPU.cu
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-#include <mem.h>;
+#include <HandlingImgOnGPU.h>
 
 #include <cuda.h>
 
@@ -39,38 +39,46 @@
  *newHeight - height of new image
  *newWidth - width of new image
  */
-__global__ void cuttngIMG(void* input_ptr, void** arrOfPtr, int size, int height, int width, int newHeight, int newWidth)
+__global__ void cuttingIMG(uchar4*** input_ptr, uchar4*** arrOfPtr, int newHeight, int newWidth)
 {
 	int NumberThread = blockIdx.x * blockDim.x + threadIdx.x; //linear address of thread
-	int SizeNewImg = newHeight * newWidth * size; //size of new picture
-	void* SrcElement = input_ptr + blockIdx.x * SizeNewImg + newWidth * size * threadIdx; //input pointer + all blocks which are highly + all elements which are rightly
-	void* NewElement = *(arrOfPtr + NumberThread * sizeof(void*));
+
+	uchar4** NewArr = arrOfPtr[NumberThread]; //pointer to new array
+
 	for(int i = 0; i<newHeight; i++)
 	{
 		for(int j = 0; j<newWidth; j++)
 		{
-			memCpy()
-			NewElement += size;
-			SrcElement += size;
+			NewArr[i][j] = *(input_ptr) [blockIdx.x * newHeight + i][threadIdx.x * newWidth + j];
 		}
 	}
 }
 
-void loadtoGPUmem(void* d_ptr, int size, int height, int width, void* src)
+void loadtoGPUmem(uchar4*** d_ptr, int height, int width, uchar4*** src)
 {
-	cudaMalloc(&d_ptr, size*height*width);
-	cudaMemcpy(&d_ptr, src, size*height*width, cudaMemcpyHostToDevice);
+	cudaMalloc(*d_ptr, sizeof(uchar4*)*height);
+	for(int i = 0; i<height; i++)
+	{
+		cudaMalloc(d_ptr[i], sizeof(uchar4) * width);
+		cudaMemcpy(d_ptr[i], (*src)[i], sizeof(uchar4) * width, cudaMemcpyHostToDevice);
+	}
 	return;
 }
 
-void deleteFromGPUMem(void* d_ptr)
+void deleteFromGPUMem(uchar4*** d_ptr, int height, int width)
 {
+	uchar4** Arr = *d_ptr;
+	for(int i = 0; i<height; i++)
+	{
+		cudaFree(Arr[i]);
+	}
+	cudaFree(Arr);
 	cudaFree(d_ptr);
 }
 
-void cutImg(void* d_ptr, void** arrOfPointers, int height, int width, int numbersPatchesH, int numbersPatchesW, int size)
+void cutImg(uchar4*** d_ptr, uchar4*** arrOfPointers, int height, int width, int numbersPatchesH, int numbersPatchesW)
 {
-	if((height % numbersPathchesH) != 0)//1) проверяем делимость размеров на число фрагментов
+	if((height % numbersPatchesH) != 0)//1) проверяем делимость размеров на число фрагментов
 		return;
 	if((width % numbersPatchesW) != 0)
 		return;
@@ -78,10 +86,16 @@ void cutImg(void* d_ptr, void** arrOfPointers, int height, int width, int number
 	int newSizeH = height/numbersPatchesH;
 	int newSizeW = width/numbersPatchesW;
 	int sizeArrOfPointers = numbersPatchesH * numbersPatchesW;
+	cudaMalloc(&(arrOfPointers), sizeof(uchar4**) * sizeArrOfPointers);
 	for(int i = 0; i<sizeArrOfPointers; i++)//2) выделяем память, записывая указатели в массив
 	{
-		cudaMalloc(&(arrOfPointers + i * sizeof(void*)), newSizeH*newSizeW*size);
+		cudaMalloc(&(arrOfPointers[i]), sizeof(uchar4*) * newSizeH);
+		for(int j = 0; j<newSizeH; j++)
+		{
+			cudaMalloc(&(arrOfPointers[i][j]), sizeof(uchar4)*newSizeW);
+		}
 	}
+	cuttingIMG<<<numbersPatchesH, numbersPatchesW>>>(d_ptr, arrOfPointers, newSizeH, newSizeW);
 
 	//3) запускаем копирование
 }
--- HandlingImgOnGPU.h
+++ HandlingImgOnGPU.h
@@ -8,7 +8,8 @@
 #ifndef HANDLINGIMGONGPU_H_
 #define HANDLINGIMGONGPU_H_
 
-void loadtoGPUmem(void*, int, int, int, void*);
-void deleteFromGPUMem(void*);
-
+void loadtoGPUmem(uchar4*** d_ptr, int height, int width, uchar4*** src);
+void deleteFromGPUMem(uchar4*** d_ptr, int height, int width);
+void cutImg(uchar4*** d_ptr, uchar4*** arrOfPointers, int height, int width, int numbersPatchesH, int numbersPatchesW);
+__global__ void cuttngIMG(uchar4*** input_ptr, uchar4*** arrOfPtr, int newHeight, int newWidth);
 #endif /* HANDLINGIMGONGPU_H_ */
